4d43448bdc586c93cc49de1943436c10188068e6
socrat89 <socratalnajjad89@gmail.com>
2021-06-14 16:39:42 +0300

    Step 7 - READ ONE

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const port = 3000;
 const today = new Date();
 var t = today.getHours() + ":" + today.getMinutes();
 const movies = [
-    { title: 'Jaws', year: 1975, rating: 8 },
-    { title: 'Avatar', year: 2009, rating: 7.8 },
-    { title: 'Brazil', year: 1985, rating: 8 },
-    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
-]
+  { title: "Jaws", year: 1975, rating: 8 },
+  { title: "Avatar", year: 2009, rating: 7.8 },
+  { title: "Brazil", year: 1985, rating: 8 },
+  { title: "الإرهاب والكباب‎", year: 1992, rating: 6.2 },
+];
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Ok");
@@ -38,42 +38,56 @@ app.get("/search", (req, res) => {
       status: 500,
       error: true,
       message: "you have to provide a search",
-    });moviesmovies
-
+    });
+  moviesmovies;
 });
 app.get("/test/movies/create", (req, res) => {
-    res.send({ status: 200, message: "create" });
+  res.send({ status: 200, message: "create" });
 });
 app.get("/test/movies/read", (req, res) => {
-    res.send({ status: 200, message: "read" ,data:movies});
+  res.send({ status: 200, message: "read", data: movies });
 });
 app.get("/test/movies/update", (req, res) => {
-    res.send({ status: 200, message: "update" });
+  res.send({ status: 200, message: "update" });
 });
 app.get("/test/movies/delete", (req, res) => {
-    res.send({ status: 200, message: "delete" });
+  res.send({ status: 200, message: "delete" });
 });
 
-app.get("/movies/read/by-date", function(req, res){
-    movies.sort(function(a,b){
-        return b.year-a.year;
-    });
-    // res.send({ status: 200, message:"ordered by Date"})
-    res.send({ status: 200, message: movies });
+app.get("/movies/read/by-date", function (req, res) {
+  movies.sort(function (a, b) {
+    return b.year - a.year;
+  });
+  // res.send({ status: 200, message:"ordered by Date"})
+  res.send({ status: 200, message: movies });
 });
 
-app.get("/movies/read/by-rate", function(req, res){
-    movies.sort(function(a,b){
-        return b.rating-a.rating;
-    });
-    res.send({ status: 200, message: movies });
+app.get("/movies/read/by-rate", function (req, res) {
+  movies.sort(function (a, b) {
+    return b.rating - a.rating;
+  });
+  res.send({ status: 200, message: movies });
 });
 
-app.get("/movies/read/by-title", function(req, res){
-    movies.sort(function(a,b){
-        if(a.title<b.title) return -1;
-        if(a.title>b.title) return 1;
-        return 0;
+app.get("/movies/read/by-title", function (req, res) {
+  movies.sort(function (a, b) {
+    if (a.title < b.title) return -1;
+    if (a.title > b.title) return 1;
+    return 0;
+  });
+  res.send({ status: 200, message: movies });
+});
+
+app.get("/movies/read/id/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  if (id <= movies.length - 1 && id > 0) {
+    res.send({ status: 200, data: movies[id] });
+  } else {
+    res.status(404).send({
+      status: 404,
+      error: true,
+      message: "the movie <ID> does not exist",
     });
-    res.send({ status: 200, message: movies });
+  }
 });
