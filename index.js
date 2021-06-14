8a4e13fd69dc7f76e8c25926c6aeca21e62a8729
socrat89 <socratalnajjad89@gmail.com>
2021-06-14 10:43:09 +0300

    Create an express simple API

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000;
+const today=new Date();
+var t=today.getHours()+":"+today.getMinutes();
 
 app.get('/', (req, res) => {
   res.send('Ok')
@@ -9,3 +11,9 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+app.get('/test', (req, res) => {
+    res.send({status:200, message:"ok"})
+  })
+  app.get('/time', (req, res) => {
+    res.send({status:200, message:t})
+  })
